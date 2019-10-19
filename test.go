ff1db54462afce4cc7a13e14c6ccbc8e32f9d58c
giantpromove <giantpromove3@mail.ru>
2019-10-19 13:08:48 +0600

    updated

---
+++ test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	student "./student"
+)
+
+func main() {
+	//student.Raid1a(-1, 6)
+	//student.Raid1a(5, 1)
+	//student.Raid1a(1, 1)
+	//student.Raid1a(1, 5)
+
+	student.Raid1d(-1, 3)
+	//student.Raid1c(-2, 1)
+	//student.Raid1d(-2, 1)
+	//student.Raid1e(-2, 5)
+
+	//student.Raid1e(5, 3)
+	//student.Raid1e(5, 1)
+	//student.Raid1e(1, 1)
+	//student.Raid1e(1, 5)
+
+}
